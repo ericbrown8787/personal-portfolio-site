1f462270ca66781bde36c4df85cafb17014bc64b
Eric Brown <eric.brown8787@gmail.com>
2024-03-18 16:04:20 -0400

    Edit eleventy config file

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('bundle.css');
   eleventyConfig.addPassthroughCopy('bundle.js');
   eleventyConfig.addPassthroughCopy('logo.svg');
+  eleventyConfig.setServerOptions({
+    showVersion: true,
+    showAllHosts: true,
+    enabled: true,
+    domdiff: false,
+  });
 };
